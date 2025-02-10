e23f3f312480e17c7eed87d722806a1e74c24a80
Tushar <tusharkaler450@gmail.com>
2025-02-11 02:00:28 +0530

    stories

--- src/Components/Stories.tsx
+++ src/Components/Stories.tsx
@@ -3,9 +3,8 @@ import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import StoriesCarouselTItem from "./StoriesCarouselTItem";
 type StoriesCarouselDataType = Array<{
-  topRated: boolean;
   name: string;
-  location: string;
+  about: string;
   experience: string;
   admissions: string;
   image: string;
@@ -16,67 +15,34 @@ export default function Stories() {
   const [scrolling, setScrolling] = useState<boolean>(false);
   const StoriesCarouselData: StoriesCarouselDataType = [
     {
-      topRated: true,
-      name: "Maya Desai",
-      location: "Pune, India",
+      name: "CA Ramesh Bajaj",
+      about: "RB & Co.",
       experience:
-        "Gyanplug turned my college application stress into a streamlined process. The guidance I received was exceptional, leading me directly to the perfect arts program that aligned with my creative aspirations.",
+        "Makes CMA in just one click , no need to apply much of our efforts.",
       admissions: "Admissions 1",
-      image: "/images/SS/1.jpg",
+      image: "/avatar.jpg",
     },
     {
-      topRated: false,
-      name: "Arjun Reddy",
-      location: "Bangalore, India",
-      experience:
-        "With Gyanplug's help, I was able to navigate the complexities of applying to technology-focused universities. Their insightful counseling helped me secure a spot at a top tech institution.",
+      name: "CA soumitra Maheshwari ",
+      about: "Soumitra & Co.",
+      experience: "Really quicckk.",
       admissions: "Admissions 2",
-      image: "/images/SS/2.jpg",
+      image: "/avatar.jpg",
     },
     {
-      topRated: true,
-      name: "Simran Kaur",
-      location: "Delhi, India",
+      name: "Pawan Kesharwani",
+      about: "Accountant",
       experience:
-        "The comprehensive support from Gyanplug, especially in preparing for my business school interviews, was crucial. I secured admissions with scholarships, thanks to their expert advice and mock sessions.",
+        "Now apart from my accounts work , I am able to make CMA for my clients",
       admissions: "Admissions 3",
-      image: "/images/SS/3.jpg",
+      image: "/avatar.jpg",
     },
     {
-      topRated: false,
-      name: "Nikhil Joshi",
-      location: "Ranchi, India",
-      experience:
-        "Gyanplug provided more than just academic guidance; they helped me find a supportive community through their club memberships. This has greatly enriched my college experience and expanded my professional network.",
+      name: "CA Ankit Gupta ",
+      about: "Ankit & Associates",
+      experience: "Quick and logical at the same time.",
       admissions: "Admissions 4",
-      image: "/images/SS/4.jpg",
-    },
-    {
-      topRated: true,
-      name: "Rohan Gupta",
-      location: "Kota, India",
-      experience:
-        "Navigating the college admission process seemed daunting until I found Gyanplug. Their Counselors provided personalized guidance that not only helped me get into my dream college but also eased my anxiety. I can't thank them enough for making this journey smoother and more successful.",
-      admissions: "Kota, India",
-      image: "/images/SS/5.jpg",
-    },
-    {
-      topRated: false,
-      name: "Anjali Singh",
-      location: "Kolkata, India",
-      experience:
-        "The internship support from Gyanplug was a game-changer for me. They connected me with an incredible summer internship opportunity in graphic design which has immensely boosted my portfolio and confidence. I now feel well-prepared for the job market thanks to their dedicated team.",
-      admissions: "Admissions 6",
-      image: "/images/SS/6.jpg",
-    },
-    {
-      topRated: true,
-      name: "Vivek Mehta",
-      location: "Chnadigarh, India",
-      experience:
-        "Choosing the right engineering college was a critical decision for me, and Gyanplug's Counselors made it so much easier. They helped me weigh my options and select a college that was the best fit for my aspirations and potential. The guidance I received was invaluable and truly tailored to my needs.",
-      admissions: "Admissions 7",
-      image: "/images/SS/7.jpg",
+      image: "/avatar.jpg",
     },
   ];
 
